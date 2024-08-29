364a508eb8f485049e0b28876c1fd5952a9a5924
Rambo7X7 <hassanchudary7x7@gmail.com>
2024-08-29 10:30:38 +0100

    update

--- db/connection.js
+++ db/connection.js
@@ -1,12 +1,13 @@
-const { Pool } = require('pg');
-const ENV = process.env.NODE_ENV || 'development';
+const { Pool } = require("pg");
+const ENV = process.env.NODE_ENV || "development";
 
-require('dotenv').config({
+require("dotenv").config({
   path: `${__dirname}/../.env.${ENV}`,
 });
 
 if (!process.env.PGDATABASE) {
-  throw new Error('PGDATABASE not set');
+  throw new Error("PGDATABASE not set");
 }
+console.log("Environment:", process.env.PGDATABASE);
 
 module.exports = new Pool();
